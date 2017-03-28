17bb3d7060aa8c2cb589eea0d04c4e7f0df4c0bb
Andrew Humble <Andrew Humble>
2017-03-28 03:41:05 -0400

    initial commit

---
+++ StoryBook/Models/HumanCharacters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoryBook.Models
+{
+    public class HumanCharacters
+    {
+        [Key]
+        public int HCID { get; set; }
+
+
+    }
+}
---
+++ StoryBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoryBook.Startup))]
+namespace StoryBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
