097c2ea78e57f039ab48acc8cf06d46434a28e00
Andrew Humble <Andrew Humble>
2017-03-28 04:32:48 -0400

    building character model

--- StoryBook/Models/HumanCharacters.cs
+++ StoryBook/Models/HumanCharacters.cs
@@ -11,6 +11,15 @@ namespace StoryBook.Models
         [Key]
         public int HCID { get; set; }
 
+        public string CharacterName { get; set; }
+        public string SkinColor { get; set; }
+        public string EyeColor { get; set; }
+        public string HairType { get; set; }
+        public string HairColor { get; set; }
+        public string ShirtColor { get; set; }
+        public string PantsColor { get; set; }
 
+        public bool Bald { get; set; }
+        public bool Freckles { get; set; }
     }
 }
