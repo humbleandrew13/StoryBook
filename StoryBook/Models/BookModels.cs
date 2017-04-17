059af17b5f96e6aa153f7d1a913da6f99e356d88
Andrew Humble <Andrew Humble>
2017-04-17 11:54:10 -0400

    began bookmodels

---
+++ StoryBook/Models/BookModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoryBook.Models
+{
+    public class BookModels
+    {
+        [Key]
+        public int BookID { get; set; }
+
+        public string BookTitle { get; set; }
+        public int BookPage { get; set; }
+
+    }
+}
